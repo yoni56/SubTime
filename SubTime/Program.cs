f5113a3ef82c3048e48b6145c3995940c6cf6a12
Jonathan D <131304022+yoni56@users.noreply.github.com>
2024-01-28 22:07:33 +0200

    .NET
    .NET 8

--- SubTime/Program.cs
+++ SubTime/Program.cs
@@ -7,11 +7,11 @@ namespace SubTime
     {
         static void Main(string[] args)
         {
-            //string fileName = args[0];
-            //int seconds = Convert.ToInt32(args[1]);
+            string fileName = args[0];
+            int seconds = Convert.ToInt32(args[1]);
 
-            string fileName = "Alex and Emma (2003).sub";
-            int seconds = 5;
+            //string fileName = "Alex and Emma (2003).sub";
+            //int seconds = 5;
 
             var subtitleFactory = new SubtitleFactory();
             ISubtitle subtitle = subtitleFactory.GetSubtitle(fileName);
